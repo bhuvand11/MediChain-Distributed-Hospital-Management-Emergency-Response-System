04682f35afbaa76be37f89f78d23e32f12e1525f
bhuvand11 <bhuvandevarakonda@gmail.com>
2026-04-06 17:12:34 +0530

    added pom.xml and all the services

---
+++ bed-service/src/main/java/com/medichain/bed_service/BedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medichain.bed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedServiceApplication.class, args);
+	}
+
+}

382112abe41a99650550d987475639711e0d5a5c
bhuvand11 <bhuvandevarakonda@gmail.com>
2026-04-18 13:03:23 +0530

    feat: phase 1 complete - multi-module maven skeleton with all 10 microservices

--- bed-service/src/main/java/com/medichain/bed_service/BedServiceApplication.java
+++ bed-service/src/main/java/com/medichain/bed_service/BedServiceApplication.java
@@ -2,12 +2,15 @@ package com.medichain.bed_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class BedServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BedServiceApplication.class, args);
 	}
-
 }
--- emergency-service/src/main/java/com/medichain/emergency_service/EmergencyServiceApplication.java
+++ emergency-service/src/main/java/com/medichain/emergency_service/EmergencyServiceApplication.java
@@ -2,12 +2,15 @@ package com.medichain.emergency_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class EmergencyServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmergencyServiceApplication.class, args);
 	}
-
 }
